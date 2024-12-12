f022c42c11f29aa61f09a850e7f6947e84512e86
Hung_HP_Ubuntu <hunghoang.1806@gmail.com>
2024-12-12 19:49:27 +0700

    [ADD] Launch file for MG Robot V10

---
+++ Dual_arm_MG_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/hung_2004/Dual_arm_CoSimulation/Dual_arm_MG_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/hung_2004/Dual_arm_CoSimulation/Dual_arm_MG_ws/build'
---
+++ Dual_arm_MG_ws/build/dual_arm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hung_2004/Dual_arm_CoSimulation/Dual_arm_MG_ws/src/dual_arm
+# Build directory: /home/hung_2004/Dual_arm_CoSimulation/Dual_arm_MG_ws/build/dual_arm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
